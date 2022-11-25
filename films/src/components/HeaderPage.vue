bd584983380382481ce043cfcebef5cd4870e6f4
qdhane <78689958+qdhane@users.noreply.github.com>
2022-11-25 14:57:51 +0300

    3

---
+++ films/src/components/HeaderPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header class="navbar">
+        <div class="logo">
+            <a href="#"><img src="../assets/logo.svg"></a>
+        </div>
+        <div class="menu-btm"></div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.logo{
+  color: black;
+}
+</style>
---
+++ films/src/views/MainPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <Header/>
+    <div class="header">
+       
+    </div>
+  </section>
+</template>
+
+<script>
+import Header from "../components/HeaderPage.vue"
+export default {
+    components:{
+        Header
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
